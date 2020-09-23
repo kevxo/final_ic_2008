447009f2e3b321cf0335921b4695cb25303191e1
Kevin Cuadros <kev040798@gmail.com>
2020-09-23 11:09:27 -0400

    Create attributes

--- lib/ingredient.rb
+++ lib/ingredient.rb
@@ -1,5 +1,8 @@
 class Ingredient
+  attr_reader :name, :unit, :calories
   def initialize(recipe)
-
+    @name = recipe[:name]
+    @unit = recipe[:unit]
+    @calories = recipe[:calories]
   end
 end
