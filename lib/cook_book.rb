1a734d35e08cf39458e7ea804f0087f2d54d9380
Kevin Cuadros <kev040798@gmail.com>
2020-09-23 12:23:10 -0400

    Create methods for ingredients and highest cal meal

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -7,4 +7,21 @@ class CookBook
   def add_recipe(recipe)
     @recipes << recipe
   end
+
+  def ingredients
+    array = []
+    @recipes.each do |recipe|
+      recipe.ingredients.each do |ingredient|
+        array << ingredient.name
+      end
+    end
+    array.uniq
+  end
+
+  def highest_calorie_meal
+    x = @recipes.max_by do |recipe|
+      recipe.total_calories
+    end
+    x
+  end
 end
