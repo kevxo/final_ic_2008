f1868aec3e6216c1af9f1fbb3de6f50f50be5cff
Kevin Cuadros <kev040798@gmail.com>
2020-09-23 11:25:44 -0400

    Finish iterartion 1 test

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -9,4 +9,22 @@ class PantryTest < Minitest::Test
 
     assert_instance_of Pantry, pantry
   end
+
+  def test_it_has_attributes_and_stock
+    pantry = Pantry.new
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+
+    assert_equal ({}), pantry.stock
+    assert_equal 0, pantry.stock_check(ingredient1)
+
+    pantry.restock(ingredient1, 5)
+    pantry.restock(ingredient1, 10)
+
+    assert_equal 15, pantry.stock_check(ingredient1)
+
+    pantry.restock(ingredient2, 7)
+
+    assert_equal 7, pantry.stock_check(ingredient2)
+  end
 end
